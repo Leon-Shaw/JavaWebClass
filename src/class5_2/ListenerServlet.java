77390ec2cf5d85be3481c98bf7b97d7a01645400
LeonShaw <951688094@qq.com>
2019-01-06 14:55:45 +0800

    JavaWeb第5节课程第2小节的程序与截图

--- src/class5_2/ListenerServlet.java
+++ src/class5_2/ListenerServlet.java
@@ -8,15 +8,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ListenerServlet",urlPatterns = "/ListenerServlet")
+@WebServlet(name = "ListenerServlet", urlPatterns = "/ListenerServlet")
 public class ListenerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session=request.getSession();
-        session.setAttribute("username","LeonShaw");
+        HttpSession session = request.getSession();
+        session.setAttribute("username", "LeonShaw");
         response.getWriter().println("Session对象的域属性username创建成功");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+        doPost(request, response);
     }
 }
